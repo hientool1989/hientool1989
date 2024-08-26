fe1a6d92d00335f77bef13d5792f52b9d6117087
hientool1989 <09041989hien@gmail.com>
2024-08-26 21:53:08 +0700

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(0);
+system("clear");
+
+
+
+
+
+
+
+
+while (true) {
+echo "chon di ban : ";
+$chon = trim(fgets(STDIN));system('clear');
+
+
+
+
+if ($chon == 1) {
+echo"======\n";
+eval (@file_get_contents('https://raw.githubusercontent.com/hientool1989/nguyentrihien/main/bot.php'));}}
+
